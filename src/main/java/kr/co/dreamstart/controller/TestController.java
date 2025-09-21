7aa6a07789cab0772553fbf0e2e062f801bc55e1
yje0515 <155126222+yje0515@users.noreply.github.com>
2025-09-21 23:56:01 +0900

    email test
    success!

--- src/main/java/kr/co/dreamstart/controller/TestController.java
+++ src/main/java/kr/co/dreamstart/controller/TestController.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.co.dreamstart.dto.Criteria;
@@ -103,4 +105,7 @@ public class TestController {
 
 		return "payTest";
 	}
+	
+	
+
 }
