9bdd86e725b18092a82e9e22ac4c15160b1edbc4
yurisuuuuu <syuri0615i@naver.com>
2025-09-26 16:56:19 +0900

    api controller update
    api controller작성시 참고사항

---
+++ src/main/java/kr/co/dreamstart/api/EventApiController.java
@@ -0,0 +1,16 @@
+package kr.co.dreamstart.api;
+
+public class EventApiController {
+
+	/*	예시
+	 *  kr/co/dreamstart/api/EventsApiController.java
+		@RestController
+		@RequestMapping("/api/v1/events")
+		public class EventsApiController {
+  			@GetMapping public List<EventDTO> list() { ... }
+  			@GetMapping("/{id}") public EventDTO detail(@PathVariable long id) { ... }
+		}
+	 */
+	
+	
+}
